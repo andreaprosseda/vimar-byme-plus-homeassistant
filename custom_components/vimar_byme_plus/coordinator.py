aab4963983102cd6dd781b4985b1cfd485038810
Andrea Prosseda <andreaprosseda@gmail.com>
2024-07-28 02:27:01 +0200

    feat: light implementation

---
+++ custom_components/vimar_byme_plus/coordinator.py
@@ -0,0 +1,66 @@
+"""Provides the Vimar DataUpdateCoordinator."""
+
+import asyncio
+from datetime import timedelta
+import logging
+
+from requests.exceptions import ConnectTimeout, HTTPError
+from xknx import XKNX
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DEFAULT_UPDATE_INTERVAL, DOMAIN
+from .vimar.vimar_client import VimarClient
+from .vimar.vimar_data import VimarData
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class VimarDataUpdateCoordinator(DataUpdateCoordinator[VimarData]):
+    """Class to manage fetching VIMAR data."""
+
+    client: VimarClient
+    config: VimarData
+    knx: XKNX
+
+    def __init__(self, hass: HomeAssistant, data: dict[str, str]) -> None:
+        """Initialize the coordinator."""
+        self.client = VimarClient(data)
+        self.knx = XKNX()
+        interval = timedelta(seconds=DEFAULT_UPDATE_INTERVAL)
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=interval)
+
+    async def start(self):
+        """Start coordinator processes."""
+        # await self.knx.start()
+
+    async def stop(self):
+        """Stop coordinator processes."""
+        await self.knx.stop()
+
+    async def retrieve_data(self):
+        """Validate Coordinator initialization."""
+        ok = await self.client.login()
+        if not ok:
+            raise VimarLoginException
+        return self.client.get_configuration_dict()
+
+    async def _async_update_data(self) -> VimarData:
+        """Fetch data from API endpoint."""
+        _LOGGER.info("Updating coordinator")
+        try:
+            async with asyncio.timeout(15):
+                return await self.hass.async_add_executor_job(self._update_data)
+        except (ConnectTimeout, HTTPError) as error:
+            raise UpdateFailed(f"Invalid response from API: {error}") from error
+
+    def _update_data(self) -> VimarData:
+        _LOGGER.debug("Obtaining data")
+        config_dict = self.client.get_configuration_dict()
+        self.config = VimarData(config_dict)
+        return self.config
+
+
+class VimarLoginException(BaseException):
+    """Exception class to handle login error."""
---
+++ custom_components/vimar_byme_plus/light.py
@@ -0,0 +1,112 @@
+"""Platform for light integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from xknx import XKNX
+from xknx.devices import Light
+
+from homeassistant.components.light import ColorMode, LightEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DATA_COORDINATOR, DOMAIN
+from .coordinator import VimarDataUpdateCoordinator
+from .vimar.vimar_application import VimarApplication
+from .vimar.vimar_dpt_values import DptValue
+from .vimar.vimar_entity import VimarEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up component based on a config entry."""
+    hass_entry = hass.data[DOMAIN][entry.entry_id]
+    coordinator: VimarDataUpdateCoordinator = hass_entry[DATA_COORDINATOR]
+    apps = coordinator.config.get_lights()
+    entities = [VimarLight(coordinator, app) for app in apps]
+    _LOGGER.debug("Lights found: %s", len(entities))
+    async_add_entities(entities, True)
+
+
+class VimarLight(VimarEntity, LightEntity):
+    """Provides a Vimar light."""
+
+    _device: Light
+
+    def __init__(
+        self, coordinator: VimarDataUpdateCoordinator, app: VimarApplication
+    ) -> None:
+        """Initialize the light."""
+        VimarEntity.__init__(self, coordinator, app)
+        self._device = self._register_knx_light(coordinator.knx)
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if the entity is on."""
+        return self._device.state is True
+
+    @property
+    def brightness(self) -> int | None:
+        """Return Brightness of this light between 0..255."""
+        return self._device.brightness
+
+    @property
+    def rgb_color(self) -> tuple[int, int, int] | None:
+        """Return RGB colors."""
+        return self._device.rgbw
+
+    @property
+    def hs_color(self) -> tuple[float, float] | None:
+        """Return the hue and saturation."""
+        return self._device.current_hs_color
+
+    @property
+    def color_mode(self) -> ColorMode:
+        """Return the color mode of the light."""
+        if self._device.supports_xyy_color:
+            return ColorMode.XY
+        if self._device.supports_hs_color:
+            return ColorMode.HS
+        if self._device.supports_rgbw:
+            return ColorMode.RGBW
+        if self._device.supports_color:
+            return ColorMode.RGB
+        if (
+            self._device.supports_color_temperature
+            or self._device.supports_tunable_white
+        ):
+            return ColorMode.COLOR_TEMP
+        if self._device.supports_brightness:
+            return ColorMode.BRIGHTNESS
+        return ColorMode.ONOFF
+
+    @property
+    def supported_color_modes(self) -> set[ColorMode]:
+        """Flag supported color modes."""
+        return {self.color_mode}
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the Vimar light on."""
+        await self._device.set_on()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the light off."""
+        await self._device.set_off()
+
+    def _register_knx_light(self, knx: XKNX) -> Light:
+        light = Light(
+            knx,
+            name=self.app.label,
+            group_address_switch=self._get_address(DptValue.ON_OFF),
+            group_address_switch_state=self._get_address(DptValue.ON_OFF_STATE),
+        )
+        knx.devices.add(light)
+        return light
