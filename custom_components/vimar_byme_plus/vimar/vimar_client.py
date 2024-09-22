dd060a51da2995f0d1fd637a23b018c3d63bbd89
Andrea Prosseda <andreaprosseda@gmail.com>
2024-09-22 21:02:30 +0200

    feat: get vimar config from file

--- custom_components/vimar_byme_plus/vimar/vimar_client.py
+++ custom_components/vimar_byme_plus/vimar/vimar_client.py
@@ -2,8 +2,8 @@
 
 import logging
 
-from ..utils.parse_xml import parse_config_from_file
-from .vimar_config import CONFIG
+from ..utils.parse_xml import parse_config_from_path
+from .model.byme_configuration.byme_configuration import ByMeConfiguration
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,26 +19,23 @@ class VimarClient:
         self.username = data.get("username")
         self.password = data.get("password")
 
-    async def login(self):
+    async def login(self) -> dict:
         """Login flow with Vimar."""
         _LOGGER.debug("Logging with Vimar credentials")
         response = {"username": "username!"}
         _LOGGER.info("Login completed")
         return response
 
-    def get_configuration(self):
+    def get_configuration(self) -> ByMeConfiguration:
         """Mock config retrieve."""
-        # path = "/workspaces/core/homeassistant/components/vimar/data/vimar_config.xml"
-        # return parse_config(path)
-        return parse_config_from_file(self.get_config())
+        path = "vimar_config.xml"
+        return parse_config_from_path(path)
+        # return parse_config_from_file(self.get_config())
 
-    def get_configuration_dict(self):
+    def get_configuration_dict(self) -> dict:
         """Mock config retrieve."""
         _LOGGER.debug("Retrieving configuration")
         response = self.get_configuration()
         config = response.to_dict()
         _LOGGER.info("Configuration retrieved")
         return config
-
-    def get_config(self):
-        return CONFIG
