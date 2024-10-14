5ad02526f8e8c27627f4ea244e6b16c415432880
Andrea Prosseda <andreaprosseda@gmail.com>
2024-10-15 00:18:52 +0200

    feat: add logs on file

--- vimar_connection/utils/logger.py
+++ vimar_connection/utils/logger.py
@@ -1,14 +1,22 @@
 import logging
+from .file import get_file_path, remove_file
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+level = logging.INFO
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(level)
+stream_handler.setFormatter(formatter)
+
+remove_file('app.log')
+file_handler = logging.FileHandler(get_file_path('app.log'), mode='a')
+file_handler.setLevel(level)
+file_handler.setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
+logger.setLevel(level)
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 def log_info(__name__: str, message: str):
     logger.name = __name__
