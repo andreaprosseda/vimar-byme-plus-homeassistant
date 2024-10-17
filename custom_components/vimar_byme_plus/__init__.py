ba97dd1ed6677e2e0b2eb9597941eb12198a9ea9
Andrea Prosseda <andreaprosseda@gmail.com>
2024-10-17 12:44:24 +0200

    feat: direct integration for light

--- custom_components/vimar_byme_plus/__init__.py
+++ custom_components/vimar_byme_plus/__init__.py
@@ -2,75 +2,37 @@
 
 from __future__ import annotations
 
-import logging
-
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DATA_COORDINATOR, DATA_UNDO_UPDATE_LISTENER, DOMAIN
 from .coordinator import VimarDataUpdateCoordinator
+from .vimar.utils.logger import log_debug
 
-PLATFORMS: list[Platform] = [Platform.CLIMATE, Platform.COVER, Platform.LIGHT]
-_LOGGER = logging.getLogger(__name__)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up Vimar integration."""
-    _LOGGER.debug("async_setup started")
-    hass.data.setdefault(DOMAIN, {})
-
-    if hass.config_entries.async_entries(DOMAIN):
-        return True
-    context = {"source": SOURCE_IMPORT}
-    data = config[DOMAIN]
-
-    if data.get("username"):
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(DOMAIN, context=context, data=data)
-        )
-    return True
+PLATFORMS: list[Platform] = [
+    Platform.LIGHT,
+    # Platform.CLIMATE,
+    # Platform.COVER,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Vimar from a config entry."""
-    _LOGGER.debug("async_setup_entry started")
-    if not entry.options:
-        options = {"username": "usr", "password": "psw"}
-        hass.config_entries.async_update_entry(entry, options=options)
-
-    coordinator = VimarDataUpdateCoordinator(hass, entry.options)
-    await coordinator.start()
+    log_debug(__name__, "Method 'async_setup_entry' started")
+    coordinator = VimarDataUpdateCoordinator(hass)
     await coordinator.async_config_entry_first_refresh()
+    await coordinator.start()
 
-    undo_listener = entry.add_update_listener(_async_update_listener)
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {
-        DATA_COORDINATOR: coordinator,
-        DATA_UNDO_UPDATE_LISTENER: undo_listener,
-    }
-
+    entry.runtime_data = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    _LOGGER.debug("async_unload_entry started")
+    log_debug(__name__, "Method 'async_unload_entry' started")
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
     if unload_ok:
-        hass.data[DOMAIN][entry.entry_id][DATA_UNDO_UPDATE_LISTENER]()
-        await hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR].stop()
-        hass.data[DOMAIN].pop(entry.entry_id)
-        hass.data.pop(DOMAIN)
-
+        coordinator: VimarDataUpdateCoordinator = entry.runtime_data
+        coordinator.stop()
     return unload_ok
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    _LOGGER.debug("_async_update_listener started")
-    await hass.config_entries.async_reload(entry.entry_id)
--- custom_components/vimar_byme_plus/light.py
+++ custom_components/vimar_byme_plus/light.py
@@ -5,19 +5,16 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from xknx import XKNX
-from xknx.devices import Light
-
 from homeassistant.components.light import ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DATA_COORDINATOR, DOMAIN
 from .coordinator import VimarDataUpdateCoordinator
-from .vimar.model.vimar_application import VimarApplication, VimarType
-from .vimar.enum.vimar_dpt_values import DptValue
-from .vimar.vimar_entity import VimarEntity
+from .middleware.vimar_entity import VimarEntity
+from .vimar.model.enum.component_type import ComponentType
+from .vimar.model.repository.user_component import UserComponent
+from .vimar.utils.logger import log_debug
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,64 +25,44 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up component based on a config entry."""
-    hass_entry = hass.data[DOMAIN][entry.entry_id]
-    coordinator: VimarDataUpdateCoordinator = hass_entry[DATA_COORDINATOR]
-    apps = coordinator.config.get_entities(VimarType.LIGHT)
-    entities = [VimarLight(coordinator, app) for app in apps]
-    _LOGGER.debug("Lights found: %s", len(entities))
+    coordinator: VimarDataUpdateCoordinator = entry.runtime_data
+    components = coordinator.get_components(ComponentType.LIGHT)
+    entities = [VimarLight(coordinator, component) for component in components]
     async_add_entities(entities, True)
 
 
 class VimarLight(VimarEntity, LightEntity):
     """Provides a Vimar light."""
 
-    _device: Light
-
     def __init__(
-        self, coordinator: VimarDataUpdateCoordinator, app: VimarApplication
+        self, coordinator: VimarDataUpdateCoordinator, component: UserComponent
     ) -> None:
         """Initialize the light."""
-        VimarEntity.__init__(self, coordinator, app)
-        self._device = self._register_knx_device(coordinator.knx)
+        VimarEntity.__init__(self, coordinator, component)
 
     @property
     def is_on(self) -> bool:
         """Return True if the entity is on."""
-        return self._device.state is True
+        return self.get_element("SFE_State_OnOff") == "On"
 
     @property
     def brightness(self) -> int | None:
         """Return Brightness of this light between 0..255."""
-        return self._device.brightness
+        return 255
 
     @property
     def rgb_color(self) -> tuple[int, int, int] | None:
         """Return RGB colors."""
-        return self._device.rgbw
+        return None
 
     @property
     def hs_color(self) -> tuple[float, float] | None:
         """Return the hue and saturation."""
-        return self._device.current_hs_color
+        return None
 
     @property
     def color_mode(self) -> ColorMode:
         """Return the color mode of the light."""
-        if self._device.supports_xyy_color:
-            return ColorMode.XY
-        if self._device.supports_hs_color:
-            return ColorMode.HS
-        if self._device.supports_rgbw:
-            return ColorMode.RGBW
-        if self._device.supports_color:
-            return ColorMode.RGB
-        if (
-            self._device.supports_color_temperature
-            or self._device.supports_tunable_white
-        ):
-            return ColorMode.COLOR_TEMP
-        if self._device.supports_brightness:
-            return ColorMode.BRIGHTNESS
         return ColorMode.ONOFF
 
     @property
@@ -95,18 +72,8 @@ class VimarLight(VimarEntity, LightEntity):
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the Vimar light on."""
-        await self._device.set_on()
+        # await self._device.set_on()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the light off."""
-        await self._device.set_off()
-
-    def _register_knx_device(self, knx: XKNX) -> Light:
-        light = Light(
-            knx,
-            name=self.app.label,
-            group_address_switch=self._get_address(DptValue.ON_OFF),
-            group_address_switch_state=self._get_address(DptValue.ON_OFF_STATE),
-        )
-        knx.devices.add(light)
-        return light
+        # await self._device.set_off()
