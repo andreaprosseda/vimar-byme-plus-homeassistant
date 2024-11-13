b1450380e9b959d6bd5ae0f0eeb3109f60dac0d5
Andrea Prosseda <andreaprosseda@gmail.com>
2024-11-13 11:30:05 +0100

    fix: brightness digit value

--- custom_components/vimar_byme_plus/vimar/mapper/light/ss_light_dimmer_mapper.py
+++ custom_components/vimar_byme_plus/vimar/mapper/light/ss_light_dimmer_mapper.py
@@ -35,7 +35,7 @@ class SsLightDimmerMapper:
 
     def get_brightness(self, component: UserComponent) -> int | None:
         value = component.get_value(SfeType.STATE_BRIGHTNESS)
-        if value.isdigit:
+        if value.isdigit():
             return int(value)
         return None  # Change up/Change down
 
