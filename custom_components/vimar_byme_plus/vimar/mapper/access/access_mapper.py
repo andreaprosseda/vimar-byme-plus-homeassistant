7894bb6909634f084861faf2031abed9a8d208ae
Andrea Prosseda <andreaprosseda@gmail.com>
2024-11-27 20:51:52 +0100

    fix: base access mapper

--- custom_components/vimar_byme_plus/vimar/mapper/access/access_mapper.py
+++ custom_components/vimar_byme_plus/vimar/mapper/access/access_mapper.py
@@ -1,49 +1,28 @@
+from .ss_access_door_window_mapper import SsAccessDoorWindowMapper
+from .ss_access_gate_mapper import SsAccessGateMapper
+from ..base_mapper import BaseMapper
 from ...model.repository.user_component import UserComponent
-from ...model.component.vimar_cover import VimarCover, CoverEntityFeature
+from ...model.component.vimar_cover import VimarCover
 from ...model.enum.sftype_enum import SfType
-from ...model.enum.sfetype_enum import SfeType
-
-SFTYPE = SfType.ACCESS.value
 
 
 class AccessMapper:
     @staticmethod
     def from_list(components: list[UserComponent]) -> list[VimarCover]:
-        return [AccessMapper.from_obj(c) for c in components if c.sftype == SFTYPE]
+        sftype = SfType.ACCESS.value
+        shutters = [component for component in components if component.sftype == sftype]
+        return [AccessMapper.from_obj(shutter) for shutter in shutters]
 
     @staticmethod
     def from_obj(component: UserComponent, *args) -> VimarCover:
-        return VimarCover(
-            id=component.idsf,
-            name=component.name,
-            device_group=component.sftype,
-            device_name=component.sstype,
-            device_class=AccessMapper.device_class(component),
-            area=component.ambient.name,
-            current_cover_position=AccessMapper.current_position(component),
-            is_closed=AccessMapper.is_closed(component),
-            is_closing=False,
-            is_opening=False,
-            supported_features=AccessMapper.get_supported_features(component),
-        )
+        mapper = AccessMapper.get_mapper(component)
+        return mapper.from_obj(component, *args)
 
     @staticmethod
-    def device_class(component: UserComponent) -> str:
-        if component.sstype == "SS_Access_Gate":
-            return "gate"
-        return "door"
-
-    @staticmethod
-    def current_position(component: UserComponent) -> int | None:
-        value = component.get_value(SfeType.STATE_ON_OFF)
-        return 100 if value == "Off" else 0
-
-    @staticmethod
-    def is_closed(component: UserComponent) -> bool | None:
-        value = component.get_value(SfeType.STATE_ON_OFF)
-        return value == "Off" if value else None
-
-    @staticmethod
-    def get_supported_features(component: UserComponent) -> list[CoverEntityFeature]:
-        """Flag media player features that are supported."""
-        return [CoverEntityFeature.OPEN]
+    def get_mapper(component: UserComponent) -> BaseMapper:
+        sstype = component.sstype
+        if sstype == SsAccessGateMapper.SSTYPE:
+            return SsAccessGateMapper()
+        if sstype == SsAccessDoorWindowMapper.SSTYPE:
+            return SsAccessDoorWindowMapper()
+        raise NotImplementedError
