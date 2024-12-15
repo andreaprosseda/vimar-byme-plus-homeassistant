f77760cf1fb267dc5c0a38c92e86ddd3fbfa480f
Andrea Prosseda <andreaprosseda@gmail.com>
2024-12-15 20:27:14 +0100

    feat: add ss_humidity implementation

---
+++ custom_components/vimar_byme_plus/vimar/mapper/sensor/ss_sensor_humidity_mapper.py
@@ -0,0 +1,39 @@
+from decimal import Decimal
+
+from ...model.component.vimar_sensor import SensorDeviceClass, VimarSensor
+from ...model.enum.sfetype_enum import SfeType
+from ...model.enum.sstype_enum import SsType
+from ...model.repository.user_component import UserComponent
+
+
+class SsSensorHumidityMapper:
+    SSTYPE = SsType.SENSOR_HUMIDITY.value
+
+    def from_obj(self, component: UserComponent, *args) -> list[VimarSensor]:
+        return [self._from_obj(component, *args)]
+
+    def _from_obj(self, component: UserComponent, *args) -> VimarSensor:
+        return VimarSensor(
+            id=component.idsf,
+            name=component.name,
+            device_group=component.sftype,
+            device_name=component.sstype,
+            device_class=SensorDeviceClass.HUMIDITY,
+            area=component.ambient.name,
+            main_id=component.idsf,
+            native_value=self.native_value(component),
+            last_update=None,
+            decimal_precision=self.decimal_precision(component),
+            unit_of_measurement=None,
+            state_class=None,
+            options=None,
+        )
+
+    def native_value(self, component: UserComponent) -> str | Decimal | None:
+        value = component.get_value(SfeType.STATE_HUMIDITY)
+        if value:
+            return Decimal(value)
+        return None
+
+    def decimal_precision(self, component: UserComponent) -> int:
+        return 1
