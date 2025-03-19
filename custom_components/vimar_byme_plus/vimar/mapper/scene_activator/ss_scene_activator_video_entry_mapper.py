614c2368346885f14e88233349bef915f3c79a32
Andrea Prosseda <andreaprosseda@gmail.com>
2025-03-19 17:10:41 +0100

    feat: scene_activator added

--- custom_components/vimar_byme_plus/vimar/mapper/scene_activator/ss_scene_activator_video_entry_mapper.py
+++ custom_components/vimar_byme_plus/vimar/mapper/scene_activator/ss_scene_activator_video_entry_mapper.py
@@ -7,7 +7,31 @@ class SsSceneActivatorVideoEntryMapper:
     SSTYPE = SsType.SCENE_ACTIVATOR_VIDEO_ENTRY.value
 
     def from_obj(self, component: UserComponent, *args) -> list[VimarButton]:
-        return [self._from_obj(component, *args)]
+        return [
+            self.button_start_call_from_obj(component, *args),
+            self.button_end_call_from_obj(component, *args),
+        ]
 
-    def _from_obj(self, component: UserComponent, *args) -> VimarButton:
-        raise NotImplementedError
+    def button_start_call_from_obj(
+        self, component: UserComponent, *args
+    ) -> VimarButton:
+        return VimarButton(
+            id=str(component.idsf) + "_start_call",
+            name=component.name + " - " + "Avvio Chiamata",
+            device_group=component.sftype,
+            device_name=component.sstype,
+            device_class=None,
+            area=component.ambient.name,
+            main_id=component.idsf,
+        )
+
+    def button_end_call_from_obj(self, component: UserComponent, *args) -> VimarButton:
+        return VimarButton(
+            id=str(component.idsf) + "_end_call",
+            name=component.name + " - " + "Fine Chiamata",
+            device_group=component.sftype,
+            device_name=component.sstype,
+            device_class=None,
+            area=component.ambient.name,
+            main_id=component.idsf,
+        )
