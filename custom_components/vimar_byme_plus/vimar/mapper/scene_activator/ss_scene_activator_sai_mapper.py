46b1d8dd5490e1ee792337243a2a380cf43bd906
Andrea Prosseda <andreaprosseda@gmail.com>
2026-02-07 00:47:50 +0100

    Update ss_scene_activator_sai_mapper.py

--- custom_components/vimar_byme_plus/vimar/mapper/scene_activator/ss_scene_activator_sai_mapper.py
+++ custom_components/vimar_byme_plus/vimar/mapper/scene_activator/ss_scene_activator_sai_mapper.py
@@ -1,13 +1,27 @@
-from ...model.component.vimar_button import VimarButton
+from ...model.component.vimar_alarm import VimarAlarm
 from ...model.enum.sstype_enum import SsType
 from ...model.repository.user_component import UserComponent
 
 
 class SsSceneActivatorSaiMapper:
+    """SS_SceneActivator_Sai: Inserimento allarme (SAI alarm panel).
+
+    Maps to a single Home Assistant alarm_control_panel entity.
+    SFEs from Specifiche.out.html: AreaDis, AreaOn, AreaInt, AreaPar,
+    AreaAlarm, AreaAlarmMemory, AreaAlarmMemoryReset, AreaAlarmReset.
+    """
+
     SSTYPE = SsType.SCENE_ACTIVATOR_SAI.value
 
-    def from_obj(self, component: UserComponent, *args) -> list[VimarButton]:
+    def from_obj(self, component: UserComponent, *args) -> list[VimarAlarm]:
         return [self._from_obj(component, *args)]
 
-    def _from_obj(self, component: UserComponent, *args) -> VimarButton:
-        raise NotImplementedError
+    def _from_obj(self, component: UserComponent, *args) -> VimarAlarm:
+        return VimarAlarm(
+            id=str(component.idsf),
+            name=component.name,
+            device_group=component.sftype,
+            device_name=component.sstype,
+            device_class=None,
+            area=component.ambient.name,
+        )
